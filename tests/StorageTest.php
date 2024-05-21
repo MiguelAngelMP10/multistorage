db70c4b58233b0c9ce3abbf1ce2cfb8e79f8cbc6
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2024-05-20 21:02:19 -0600

    success error

--- tests/StorageTest.php
+++ tests/StorageTest.php
@@ -1,73 +1,75 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use Mockery as m;
 use MiguelAngelMP10\Multistorage\Interfaces\StorageInterface;
 
 class StorageTest extends TestCase {
-    use MockeryPHPUnitIntegration;
 
-    protected function tearDown(): void {
-        m::close();
+    public function testLocalStorage(): void {
+        // Crear un mock de la interfaz
+        $storageMock = $this->createMock(StorageInterface::class);
+
+        // Configurar la expectativa de que 'writeFile' sea llamado una vez con los parámetros especificados
+        $storageMock->expects($this->once())
+                    ->method('writeFile')
+                    ->with('test.txt', 'Hello, World!')
+                    ->willReturn(true);
+
+        // Llamar al método y hacer la aserción
+        $this->assertTrue($storageMock->writeFile('test.txt', 'Hello, World!'));
     }
 
-    public function testLocalStorage() {
-        $storageMock = m::mock(StorageInterface::class);
-        $storageMock->shouldReceive('writeFile')
-                    ->once()
+    public function testSftpStorage(): void {
+        $storageMock = $this->createMock(StorageInterface::class);
+
+        $storageMock->expects($this->once())
+                    ->method('writeFile')
                     ->with('test.txt', 'Hello, World!')
-                    ->andReturn(true);
+                    ->willReturn(true);
 
         $this->assertTrue($storageMock->writeFile('test.txt', 'Hello, World!'));
     }
 
-    public function testSftpStorage() {
-        $storageMock = m::mock(StorageInterface::class);
-        $storageMock->shouldReceive('writeFile')
-                    ->once()
+    public function testGcpStorage(): void {
+        $storageMock = $this->createMock(StorageInterface::class);
+
+        $storageMock->expects($this->once())
+                    ->method('writeFile')
                     ->with('test.txt', 'Hello, World!')
-                    ->andReturn(true);
+                    ->willReturn(true);
 
         $this->assertTrue($storageMock->writeFile('test.txt', 'Hello, World!'));
     }
 
-    public function testGcpStorage() {
-        $storageMock = m::mock(StorageInterface::class);
-        $storageMock->shouldReceive('writeFile')
-                    ->once()
+    public function testAzureStorage(): void {
+        $storageMock = $this->createMock(StorageInterface::class);
+
+        $storageMock->expects($this->once())
+                    ->method('writeFile')
                     ->with('test.txt', 'Hello, World!')
-                    ->andReturn(true);
+                    ->willReturn(true);
 
         $this->assertTrue($storageMock->writeFile('test.txt', 'Hello, World!'));
     }
 
-    public function testAzureStorage() {
-        $storageMock = m::mock(StorageInterface::class);
-        $storageMock->shouldReceive('writeFile')
-                    ->once()
+    public function test_S3_Storage(): void {
+        $storageMock = $this->createMock(StorageInterface::class);
+
+        $storageMock->expects($this->once())
+                    ->method('writeFile')
                     ->with('test.txt', 'Hello, World!')
-                    ->andReturn(true);
+                    ->willReturn(true);
 
         $this->assertTrue($storageMock->writeFile('test.txt', 'Hello, World!'));
     }
 
-    public function test_S3_Storage() {
-        $storageMock = m::mock(StorageInterface::class);
-        $storageMock->shouldReceive('writeFile')
-                    ->once()
-                    ->with('test.txt', 'Hello, World!')
-                    ->andReturn(true);
+    public function testDigitalOceanSpacesStorage(): void {
+        $storageMock = $this->createMock(StorageInterface::class);
 
-        $this->assertTrue($storageMock->writeFile('test.txt', 'Hello, World!'));
-    }
-
-    public function testDigitalOceanSpacesStorage() {
-        $storageMock = m::mock(StorageInterface::class);
-        $storageMock->shouldReceive('writeFile')
-                    ->once()
+        $storageMock->expects($this->once())
+                    ->method('writeFile')
                     ->with('test.txt', 'Hello, World!')
-                    ->andReturn(true);
+                    ->willReturn(true);
 
         $this->assertTrue($storageMock->writeFile('test.txt', 'Hello, World!'));
     }
